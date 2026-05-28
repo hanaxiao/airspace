cde992912323703368b7bda63c9e036a8dc28a13
hanaxiao <hana_xiao@airspaceonline.com>
2026-05-28 17:57:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.API_URL = 'window.API_URL = 'https://script.google.com/macros/s/AKfycbzLLza1GFKP2dK5EpHGkwR2nNc3y1kMRBx-333wG6wDuN4pDvkfA1-v6Iks83EWtSqK/exec';';
+window.API_URL='https://script.google.com/macros/s/AKfycbzLLza1GFKP2dK5EpHGkwR2nNc3y1kMRBx-333wG6wDuN4pDvkfA1-v6Iks83EWtSqK/exec';';
